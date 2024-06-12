01a2f32192229784e960dd223860d01927e916ea
yagrariksa <luckyrama0@gmail.com>
2024-06-12 20:46:26 +0700

    init
    Signed-off-by: yagrariksa <luckyrama0@gmail.com>

---
+++ mac-todolist/mac_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_todolistApp.swift
+//  mac-todolist
+//
+//  Created by Daffa Yagrariksa on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct mac_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
